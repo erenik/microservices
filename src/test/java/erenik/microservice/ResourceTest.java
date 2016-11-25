6860afab7431332d7d3e71eb054f9429ca5d62a7
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-11-25 16:44:53 +0100

    Initial commit with working code test unit tests

---
+++ src/test/java/erenik/microservice/ResourceTest.java
@@ -0,0 +1,43 @@
+package erenik.microservice;
+  
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class ResourceTest {
+ 
+    private HttpServer server;
+    private WebTarget target;
+ 
+    @Before
+    public void setUp() throws Exception {
+        server = Main.startServer();
+ 
+        Client c = ClientBuilder.newClient();
+        target = c.target(Main.BASE_URI);
+    }
+ 
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+ 
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        String responseMsg = target.path("myresource").request().get(String.class);
+        System.out.println("Output: "+responseMsg);
+        assert(responseMsg.length() > 1);
+//        assertEquals("Got it!", responseMsg);
+    }
+}
